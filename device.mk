816143abd4e0b3efa9eed1476c7dda8d09ef1ca5
Peter Meiser <peter.meiser@gmx.com>
2025-04-09 06:56:00 +0200

    diting: enable blur

--- device.mk
+++ device.mk
@@ -13,6 +13,9 @@ $(call inherit-product, vendor/xiaomi/diting/diting-vendor.mk)
 # MIUI Camera
 $(call inherit-product, device/xiaomi/miuicamera-cupid/device.mk)
 
+# Blur
+TARGET_ENABLE_BLUR := true
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_waipio_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape/mixer_paths_waipio_mtp.xml \
