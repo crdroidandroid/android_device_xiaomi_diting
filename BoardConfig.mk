85d84f677083677f0676cebe435ce7d8195b1722
Jezzay97 <pincayjesse@gmail.com>
2025-07-11 22:13:57 -0500

    diting: Include MiuiCamera if existing

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,9 @@ include vendor/xiaomi/diting/BoardConfigVendor.mk
 
 DEVICE_PATH := device/xiaomi/diting
 
+# MiuiCamera
+-include device/xiaomi/miuicamera-diting/BoardConfig.mk
+
 # Audio
 AUDIO_FEATURE_ENABLED_CIRRUS_CALIBRATION_RESISTANCE := true
 
--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/diting/diting-vendor.mk)
 
+# MiuiCamera
+$(call inherit-product-if-exists, device/xiaomi/miuicamera-diting/device.mk)
+
 # Blur
 TARGET_ENABLE_BLUR := true
 
